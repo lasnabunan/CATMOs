992e9220a2a3770c47a57828e62a3884af8ed57a
Hritik Bansal <hritik.anselmite@gmail.com>
2021-06-19 00:45:47 +0530

    Add files via upload

---
+++ NodeMCU_DHT_Client.ino
@@ -0,0 +1,84 @@
+
+
+
+/*  Connects to the home WiFi network
+ *  Asks some network parameters
+ *  Sends and receives message from the server in every 2 seconds
+ *  Communicates: wifi_server_01.ino
+ */ 
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+
+#define DHTTYPE DHT22
+const int DHTPin = 5;
+DHT dht(DHTPin, DHTTYPE);
+
+
+byte ledPin = 2;
+byte led = D0;
+char ssid[] = "FLASH";           // SSID of your home WiFi
+char pass[] = "BARRYALLEN";   // password of your home WiFi
+
+char celsiusTemp[7];
+char fahrenheitTemp[7];
+char humidityTemp[7];
+
+unsigned long askTimer = 0;
+
+IPAddress server(192,168,43,13);       // the fix IP address of the server
+WiFiClient client;
+
+void setup() 
+{
+  dht.begin();
+  Serial.begin(115200); // only for debug
+  WiFi.begin(ssid, pass);             // connects to the WiFi router
+  while (WiFi.status() != WL_CONNECTED) 
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("Connected to wifi");
+  Serial.print("Status: "); Serial.println(WiFi.status());    // Network parameters
+  Serial.print("IP: ");     Serial.println(WiFi.localIP());
+  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
+}
+
+void loop () 
+{
+  client.connect(server, 80);   // Connection to the server
+  while(client.connected())
+  {
+    String answer = client.readStringUntil('\r');
+    if(answer.equals("")==false)
+      Serial.println("Aye Aye Captain");
+      
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    float f = dht.readTemperature(true);
+    if (isnan(h) || isnan(t) || isnan(f)) 
+    {
+      Serial.println("Failed to read from DHT sensor!");
+      strcpy(celsiusTemp,"Failed");
+      strcpy(fahrenheitTemp, "Failed");
+      strcpy(humidityTemp, "Failed");
+    }
+    else
+    {
+      float hic = dht.computeHeatIndex(t, h, false);
+      dtostrf(hic, 6, 2, celsiusTemp);
+      float hif = dht.computeHeatIndex(f, h, true);
+      dtostrf(hif, 6, 2, fahrenheitTemp);
+      dtostrf(h, 6, 2, humidityTemp);
+    }
+    client.println(celsiusTemp);
+    client.println(fahrenheitTemp);
+    client.println(humidityTemp);      
+  }
+  client.flush();       
+  
+}
---
+++ NodeMCU_DHT_Server.ino
@@ -0,0 +1,62 @@
+/*  Connects to the home WiFi network
+ *  Asks some network parameters
+ *  Starts WiFi server with fix IP and listens
+ *  Receives and sends messages to the client
+ *  Communicates: wifi_client_01.ino
+ */
+#include <SPI.h>
+#include <ESP8266WiFi.h>
+
+byte ledPin = 2;
+char ssid[] = "**********";               // SSID of your home WiFi
+char pass[] = "**********";               // password of your home WiFi
+WiFiServer server(80);                    
+
+IPAddress ip(192, 168, 43, 11);            // IP address of the server
+IPAddress gateway(192,168,43,1);           // gateway of your network
+IPAddress subnet(255,255,255,0);          // subnet mask of your network
+
+String hum="";
+String tempC="";
+String tempF="";
+
+void setup() {
+  Serial.begin(115200);                   // only for debug
+  WiFi.config(ip, gateway, subnet);       // forces to use the fix IP
+  WiFi.begin(ssid, pass);                 // connects to the WiFi router
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  server.begin();                         // starts the server
+  Serial.println("Connected to wifi");
+  Serial.print("Status: "); Serial.println(WiFi.status());  // some parameters from the network
+  Serial.print("IP: ");     Serial.println(WiFi.localIP());
+  Serial.print("Subnet: "); Serial.println(WiFi.subnetMask());
+  Serial.print("Gateway: "); Serial.println(WiFi.gatewayIP());
+  Serial.print("SSID: "); Serial.println(WiFi.SSID());
+  Serial.print("Signal: "); Serial.println(WiFi.RSSI());
+  Serial.print("Networks: "); Serial.println(WiFi.scanNetworks());
+}
+
+void loop () 
+{
+  WiFiClient client = server.available();
+  if (client) 
+  {
+    if (client.connected()) 
+    {
+      Serial.println(".");
+      client.println("Keep updating the data");
+      hum= client.readStringUntil('\r');    // receives the humidity from the client
+      tempC= client.readStringUntil('\r');  // receives the temperature in C the client
+      tempF= client.readStringUntil('\r');  // receives the temperature in F from the client
+      client.flush();
+
+      Serial.println("Humidity : "); Serial.println(hum); Serial.println("%");
+      Serial.println("Temperature : "); Serial.println(tempC); Serial.println("º");
+      Serial.println("Temperature : "); Serial.println(tempF); Serial.println("º");
+    }
+    client.stop();
+  }
+}
